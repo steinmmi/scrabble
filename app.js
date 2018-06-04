1bbe2b9e06816b9eaebf6e9fee9271276ac094f2
Mathieu Steinmetz <mathieu.steinmetz@kristal.io>
2018-06-04 17:54:44 +0200

    Ajout tour par tour

--- app.js
+++ app.js
@@ -10,19 +10,47 @@ app.engine('jade', require('jade').__express);
 app.get("/", function(req, res){
     res.render("index");
 });
+let started = false
+let players = []
+let scores = []
+
+const tabC = [[1,0,0,4,0,0,0,1,0,0,0,4,0,0,1],
+    [0,2,0,0,0,3,0,0,0,3,0,0,0,2,0],
+    [0,0,2,0,0,0,4,0,4,0,0,0,2,0,0],
+    [4,0,0,2,0,0,0,4,0,0,0,2,0,0,4],
+    [0,0,0,0,2,0,0,0,0,0,2,0,0,0,0],
+    [0,3,0,0,0,3,0,0,0,3,0,0,0,3,0],
+    [0,0,4,0,0,0,4,0,4,0,0,0,4,0,0],
+    [1,0,0,4,0,0,0,9,0,0,0,4,0,0,1],
+    [0,0,4,0,0,0,4,0,4,0,0,0,4,0,0],
+    [0,3,0,0,0,3,0,0,0,3,0,0,0,3,0],
+    [0,0,0,0,2,0,0,0,0,0,2,0,0,0,0],
+    [4,0,0,2,0,0,0,4,0,0,0,2,0,0,4],
+    [0,0,2,0,0,0,4,0,4,0,0,0,2,0,0],
+    [0,2,0,0,0,3,0,0,0,3,0,0,0,2,0],
+    [1,0,0,4,0,0,0,1,0,0,0,4,0,0,1]]
 
 const lettersValue = {A : 1,B : 3,C : 3,D : 2,E : 1,F : 4,G : 2,H : 4,I : 1,J : 8,K : 5,L : 1,M : 3,N : 1,O : 1,P : 3,Q : 10,R : 1,S : 1,T : 1,U : 1,V : 4,W : 4,X : 8,Y : 4,Z : 10}
 const MAINHAND = 7
-let users = []
+let users = {}
+
+let gameLetters = []
+for(let i = 0; i < 15; i++) {
+  gameLetters[i] = []
+}
 
 let io = require('socket.io').listen(app.listen(port));
 let playerActual = ""
 io.sockets.on('connection', function (socket) {
-    users.push(socket.id)
-    io.sockets.emit('newUser',socket.id)
+
+  socket.on('setName',function(name) {
+    users[socket.id] = name
+    io.sockets.emit('notification', {
+      message: `L'utilisateur ${users[socket.id]} vient de se connecter...`,
+      type: 'system'
+    })
+  })
     let tempLetters = []
-    for(let i = 0; i < MAINHAND ; i++)
-        sendLetter(socket)
 
     socket.on('clickBox', function (data) {
         console.log(`Clic en [${data.x},${data.y}] : ${data.letter} | ${socket.id} | ${data.caseValue}`)
@@ -36,12 +64,22 @@ io.sockets.on('connection', function (socket) {
         console.log(tempLetters)
     });
 
-    socket.on('endturn', function () {
+    socket.on('endturn', function (data) {
         let totalBonus = 1
-        let bonus = 1
         let totalPts = 0
-        for(lett of tempLetters) {
-            switch (lett.caseValue) {
+        let wordPts = 0
+        indexLetter = tempLetters[0]
+        if(data.x.length < 2 && data.y.length < 2) {
+          io.sockets.emit('cancel', tempLetters)
+          socket.emit('validate', false)
+          tempLetters = []
+          return null
+        }
+        if(data.x.length > 1)
+        for(lett of data.x) {
+          if(lett.x > indexLetter.x) indexLetter = lett
+          let bonus = 1
+            switch (tabC[lett.y][lett.x]) {
                 case 4:
                     bonus = 2
                     break
@@ -58,26 +96,125 @@ io.sockets.on('connection', function (socket) {
                     totalBonus*=2
                     break
             }
-            totalPts += bonus*lettersValue[lett.letter]
-            sendLetter(socket)
+            wordPts += bonus*lettersValue[lett.letter]
+            gameLetters[lett.y][lett.x] = lett.letter
         }
-        console.log("Le mot vaut : "+totalPts*totalBonus)
+        totalPts += wordPts*totalBonus
+        totalBonus = 1
+        wordPts = 0
+        if(data.y.length > 1)
+        for(lett of data.y) {
+          let bonus = 1
+          console.log(lett.y+ " " +lett.x)
+            switch (tabC[lett.y][lett.x]) {
+                case 4:
+                    bonus = 2
+                    break
+                case 3:
+                    bonus = 3
+                    break
+                case 1:
+                    totalBonus*=3
+                    break
+                case 2:
+                    totalBonus*=2
+                    break
+                case 9:
+                    totalBonus*=2
+                    break
+            }
+            console.log(totalBonus + " " + bonus + " : " + lett.letter)
+            wordPts += bonus*lettersValue[lett.letter]
+            gameLetters[lett.y][lett.x] = lett.letter
+        }
+        totalPts+= wordPts*totalBonus
+        console.log(totalPts)
+        for(let temp of tempLetters) sendLetter(socket.id)
         tempLetters = []
+        socket.emit('validate', true)
+
+        io.sockets.emit('notification', {
+          message: `Le joueur ${users[socket.id]} a fait un score de ${totalPts}`,
+          type: 'system'
+        })
+        scores[actualPlayer] += totalPts
+        nextTurn()
     })
     socket.on('cancel', function () {
         io.sockets.emit('cancel', tempLetters)
         tempLetters = []
     })
-    socket.on('disconnect', function() {
-        users.splice(users.indexOf(socket.id), 1);
-    });
+    socket.on('message', function(msg) {
+      if(msg.charAt(0) === '/')
+        switch (msg) {
+          case '/start':
+            if(!started)
+              start()
+            break;
+          case '/score':
+            if(started)
+              askScore()
+            break
+          default:
+            socket.emit('notification', {
+              message: `Cette commande n'existe pas, tapez /help pour plus d'information`,
+              type: 'system'
+            })
+        }
+      else {
+      io.sockets.emit('notification', {
+        message: `<b>${users[socket.id]}</b> : `+msg,
+        type: 'message'
+      })
+    }
+    })
 
+    socket.on('disconnect', function() {
+      if(users[socket.id]) {
+      io.sockets.emit('notification', {
+        message: `L'utilisateur ${users[socket.id]} vient de se déconnecter...`,
+        type: 'system'
+      })
+    }
+    delete users[socket.id]
+    });
 });
 
-function sendLetter(socket) {
+function sendLetter(socketId) {
     if(!allLetters.length) return null
     let index = Math.floor(Math.random() * allLetters.length)
     let letter = allLetters[index]
     allLetters.splice(index, 1);
-    socket.emit('addLetter', {letter : letter})
+    io.sockets.connected[ socketId ].emit('addLetter', {letter : letter})
+}
+
+function start() {
+  io.sockets.emit('notification', {
+    message: `La partie démarre ...`,
+    type: 'system'
+  })
+  for(id in users) {
+  for(let i = 0; i < MAINHAND ; i++)
+      sendLetter(id)
+  players.push(id)
+  scores.push(0)
+  }
+  actualPlayer = -1
+  started = true
+  nextTurn()
+}
+
+function askScore (socket) {
+  for(let playerId in players)
+    console.log(scores[playerId])
+}
+
+function nextTurn() {
+  actualPlayer++
+  if(actualPlayer >= players.length) actualPlayer = 0
+  io.sockets.emit('notification', {
+    message: `Au tour de `+users[players[actualPlayer]],
+    type: 'system'
+  })
+  io.sockets.emit('whosTurn',users[players[actualPlayer]])
 }
--- public/scrabble.js
+++ public/scrabble.js
@@ -1,9 +1,11 @@
 window.onload = function() {
     let socket = io.connect('http://localhost:3700');
-
+    let myName = prompt('Quel est ton pseudo ?')
+    if(!myName) return null
+    socket.emit('setName',myName)
     game = document.querySelector("#game")
     let letterActual = ''
-    const BOXSIZE = 40
+    const BOXSIZE = 50
     const BOARDSIZE = 15
     const paper = new Raphael(game, BOXSIZE*BOARDSIZE, BOXSIZE*BOARDSIZE);
     let tab = []
@@ -24,7 +26,8 @@ window.onload = function() {
         [0,2,0,0,0,3,0,0,0,3,0,0,0,2,0],
         [1,0,0,4,0,0,0,1,0,0,0,4,0,0,1]]
 
-
+    let actualWord = {}
+    let myTurn = false
 
     for(let y = 0; y < BOARDSIZE; y++ ) {
         tab[y] = []
@@ -61,9 +64,11 @@ window.onload = function() {
                 )
             imgLetters[y][x] = paper.image(``, x * BOXSIZE, y * BOXSIZE, BOXSIZE, BOXSIZE).attr({
                 fill: "black"
-            })
+            }).data('value','')
             .click(function () {
                 if (!letterActual) return null
+
+                if(!myTurn) return null
                 if(imgLetters[y][x].attr('src') !== 'about:blank' && imgLetters[y][x].attr('src') !== '') return null
                 socket.emit('clickBox', {
                     x: x,
@@ -81,7 +86,29 @@ window.onload = function() {
     socket.on('clickBox', function (data) {
         imgLetters[data.y][data.x].attr({
             src: `letters/${data.letter}.jpg`
-        });
+        }).data('value',data.letter);
+        let i = 0
+        let j = 0
+        actualWord.x = []
+        actualWord.y = []
+        while (imgLetters[data.y][data.x-i].data('value') !== '')
+          i++
+        while (imgLetters[data.y-j][data.x].data('value') !== '')
+          j++
+        let startXIndex = data.x-i+1
+        let startYIndex = data.y-j+1
+        console.log(startYIndex)
+        i = 0
+        j = 0
+      while (imgLetters[data.y][startXIndex+i].data('value') !== '') {
+          actualWord.x.push({y: data.y, x: startXIndex+i, letter: imgLetters[data.y][startXIndex+i].data('value')})
+          i++
+      }
+      while (imgLetters[startYIndex+j][data.x].data('value') !== '') {
+        actualWord.y.push({y: startYIndex+j, x: data.x, letter: imgLetters[startYIndex+j][data.x].data('value')})
+        j++
+    }
+      console.log(actualWord)
     })
     let element = document.querySelector('#letters')
 
@@ -89,9 +116,17 @@ window.onload = function() {
         createLetter(data.letter)
     })
 
+    socket.on('notification',function(data) {
+      let chatArea = document.querySelector('#textarea')
+      let message = document.createElement('p')
+      message.classList.add(data.type)
+      message.innerHTML = data.message
+      chatArea.appendChild(message)
+    })
+
     socket.on('cancel', function (data) {
         data.forEach(function (doc) {
-            imgLetters[doc.y][doc.x].attr({src: ""})
+            imgLetters[doc.y][doc.x].attr({src: ""}).data('value','')
             let fillColor =''
             switch (tabC[doc.y][doc.x]) {
                 case 3:
@@ -137,28 +172,56 @@ window.onload = function() {
             selectLetter(this)
         })
     }
+
+
     document.querySelector('#cancel').addEventListener('click',function () {
         let usedCards = document.querySelectorAll('.used')
         for (i = 0; i < usedCards.length; i++) {
             usedCards[i].classList.remove('used');
-
         }
         socket.emit('cancel')
     })
 
+    document.querySelector('#chatButton').addEventListener('click', function () {
+      socket.emit('message', document.querySelector('#chatInput').value)
+      document.querySelector('#chatInput').value = ''
+    })
     document.querySelector('#endturn').addEventListener('click',function () {
         let usedCards = document.querySelectorAll('.used')
+        if(myTurn)
+        socket.emit('endturn', actualWord)
+    })
+    socket.on('validate', function(data) {
+      let usedCards = document.querySelectorAll('.used')
+      console.log(data)
+      if(!data) {
         for (i = 0; i < usedCards.length; i++) {
-            usedCards[i].remove('used');
+            usedCards[i].classList.remove('used');
+          }
+      }
+      else {
+        for (i = 0; i < usedCards.length; i++) {
+          usedCards[i].remove('used');
         }
-        socket.emit('endturn')
+      }
+    })
+    socket.on('whosTurn', function(name) {
+      console.log('au tour de '+ name)
+      let gameArea = document.querySelector('#game')
+      if(name === myName) {
+        myTurn = true
+        gameArea.classList.remove('disabled')
+      }
+      else {
+        myTurn = false
+        gameArea.classList.add('disabled')
+      }
     })
     function selectLetter (card) {
         let elements = document.querySelectorAll('.carte')
         for (i = 0; i < elements.length; i++) {
             elements[i].classList.remove('selected');
         }
-
         card.classList.add('selected')
     }
 }
